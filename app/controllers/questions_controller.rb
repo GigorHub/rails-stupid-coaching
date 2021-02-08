92dbfb0fc02f5c0d90a671b4d0b98507c0f21b8a
Igor Murphy <igor.murphy@gmail.com>
2021-02-08 21:24:44 +0100

    Stupid coaching exco cleaned-up a little

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,19 @@
 class QuestionsController < ApplicationController
   def ask
-  #  @question = params[:question] if params[:question].present?
   end
 
   def answer
     @question = params[:question] if params[:question].present?
-    @answer =
-    if @question.downcase == 'i am going to work right now!'
-      'Great!'
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question.downcase == 'i am going to work right now!'
+      @answer = 'Great!'
+    elsif @question.downcase =~ /i am going to work/i
+      @answer = 'Great!'
     elsif @question.end_with?('?')
-      'Silly question, get dressed and go to work!'
+      @answer = 'Silly question, get dressed and go to work!'
     else
-      "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
